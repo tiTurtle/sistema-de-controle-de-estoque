550389409b49e9087e296583e353df471d2d6264
teusx97 <125636196+teusx97@users.noreply.github.com>
2024-12-29 11:16:50 -0300

    Update funcoes_principais.py
    Excluindo os comentários inúteis

--- funcoes_principais.py
+++ funcoes_principais.py
@@ -219,18 +219,6 @@ def exibir_relatorio(estoque):
     print(f"{'Faturamento Total:':<45}{faturamento_total:>32.2f}")
 
 
-
-
-
-""" atualizar_preco() """
-""" filtro_baixa_quantidade(tratar_dados_estoque(estoque_inicial)) """
-""" listar_produtos(tratar_dados_estoque(estoque_inicial)) """
-
-""" buscar_produtos() """
-
-""" print(tratar_dados_estoque(estoque_inicial))
-remover_produtos() """
-
 """ consultar_produtos_esgotados(tratar_dados_estoque(estoque_inicial)) """
 """ ordenar_produtos(tratar_dados_estoque(estoque_inicial)) """
 """ atualizar_quantidade() """
